058ccc9c91abc703c2dddc18a2777e199f929ccc
Andrew Alteri <andrewsalteri@gmail.com>
2020-08-24 18:14:12 -0500

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,13 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
+    @author = Author.new(author_params)
+    if @author.save
     redirect_to author_path(@author)
+    else  
+    @errors = @author.errors.full_messages
+    render "new"
+    end
   end
 
   private
